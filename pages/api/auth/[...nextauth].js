f274d97083630a0cf59f0cc68fa29343a22560d2
Justice Isreal <justiceyba@gmail.com>
2023-02-16 06:04:09 +0100

    bug: setting up facebook provider for live link

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,11 +2,11 @@ import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
-  baseUrl: process.env.NEXTAUTH_URL,
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackUrl: `${process.env.NEXTAUTH_URL}/api/auth/callback/facebook`,
     }),
   ],
 });
