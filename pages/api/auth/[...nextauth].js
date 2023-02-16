3b5bae6d46e3ed47c217dca3b63eed3e25f0848c
Justice Isreal <justiceyba@gmail.com>
2023-02-16 05:54:47 +0100

    app completed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
+  baseUrl: process.env.NEXTAUTH_URL,
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
