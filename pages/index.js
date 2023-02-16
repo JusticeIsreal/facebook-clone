9e831c9a5e41b9397b5b0e6957e1f889e00ac73c
Justice Isreal <justiceyba@gmail.com>
2023-02-16 06:34:39 +0100

    bug: setting up facebook provider for live link

--- pages/index.js
+++ pages/index.js
@@ -55,6 +55,10 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <meta
+          property="og:image"
+          content="https://links.papareact.com/5me"
+        ></meta>
         <title>Facebook Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
