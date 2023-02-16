6efd1b6151a93a477f194ff8c64375f10d91a908
Justice Isreal <justiceyba@gmail.com>
2023-02-16 18:43:32 +0100

    bug: setting up facebook provider for live link

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,38 +1,14 @@
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 
-const callbacks = {
-  async signIn(user, account, profile) {
-    if (account.provider === "facebook") {
-      // You can access the accessToken for the account with this:
-      // account.accessToken
-      return true;
-    }
-    // Return false to deny access
-    return false;
-  },
-  async redirect(url, baseUrl) {
-    return url.startsWith(baseUrl) ? url : baseUrl;
-  },
-};
-
 export default NextAuth({
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      scope: "email",
     }),
   ],
-  callbacks,
+  // adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
-  pages: {
-    signIn: "/auth/signin",
-  },
-  session: {
-    jwt: true,
-  },
+  nextAuthUrl: process.env.NEXTAUTH_URL,
 });
